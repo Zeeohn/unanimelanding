4314f041ca5b52a81a89195de30945958ec72be9
Zeeohn <successzion21@gmail.com>
2025-01-03 15:33:03 +0100

    Uploaded content on company culture and added links to footer

--- src/app/culture/page.jsx
+++ src/app/culture/page.jsx
@@ -39,41 +39,47 @@ const Culture = () => {
           />
           <div className="absolute top-0 w-full left-0 md:max-w-[50%] h-full flex flex-col justify-center pl-[5%] z-20">
             <p className="hidden md:block font-opensans font-bold text-[56px] leading-[70px] text-white">
-              People & Culture at BOLD
+              Discover the Bold Spirit: People & Culture at BOLD
             </p>
             <p className="md:hidden font-opensans font-bold text-[35px] text-white">
-              People & Culture at <br /> BOLD
+              Discover the Bold Spirit: <br /> People & Culture at BOLD
             </p>
             <p className="font-opensans w-full text-base md:text-xl md:font-semibold text-white pt-6">
-              Experience seamless travel with our professional cab services.
-              Whether you need a quick ride to the airport or a leisurely
-              journey across town, we prioritize your comfort and safety.
+              At Bold, our success is driven by our people. We create an
+              inclusive culture where innovation and collaboration flourish, and
+              every voice is heard. We’re shaping a future where every journey,
+              on the road or in our careers, counts. Bold ideas lead to bold
+              results. And here, everyone feels valued.
             </p>
           </div>
         </div>
 
         <div className="px-[5%] pt-8 pb-10 md:pt-20 md:pb-28">
           <p className="hidden md:block font-opensans font-bold md:text-[35px] text-center">
-            Lorem ipsum dolor sit amet{" "}
+            Empowering Bold Connections{" "}
           </p>
           <p className="md:hidden font-opensans font-bold text-[20px]">
-            Lorem ipsum dolor sit amet
+            Empowering Bold Connections
           </p>
           <p className="hidden md:block pt-10 text-black text-xl text-center">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat
+            At Bold, we are dedicated to creating connections that truly matter,
+            transforming the way people move and interact. Our customer-centric
+            approach ensures every journey is designed with safety, comfort, and
+            efficiency in mind. We are deeply committed to sustainability,
+            adopting eco-friendly practices and technologies to build a greener
+            future. Through active community engagement, we empower individuals
+            and foster opportunities for growth, connection, and success.
+            Together, we strive to make everyday experiences extraordinary by
+            delivering seamless transportation solutions and fostering a
+            collaborative, innovative work environment.
           </p>
           <p className="md:hidden pt-5 text-black text-base">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            At Bold, we are dedicated to creating connections that truly matter,
+            transforming the way people move and interact. Our customer-centric
+            approach ensures every journey is designed with safety, comfort, and
+            efficiency in mind. We are deeply committed to sustainability,
+            adopting eco-friendly practices and technologies to build a greener
+            future.
           </p>
         </div>
 
@@ -88,19 +94,21 @@ const Culture = () => {
               Our Mission
             </p>
             <p className="font-bold text-xl pt-6 md:pt-0 md:text-3xl font-opensans">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod
+              Driving Change One Ride At A Time
             </p>
             <p className="hidden md:block text-base md:text-xl text-black pt-6 font-opensans">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+              At Bold, our mission is to connect people and businesses
+              seamlessly while shaping a more sustainable, inclusive future.
+              From providing reliable transportation solutions to empowering
+              communities through technology, we’re committed to delivering
+              excellence every step of the way.{" "}
             </p>
             <p className="md:hidden text-base text-black pt-6 font-opensans">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation
+              At Bold, our mission is to connect people and businesses
+              seamlessly while shaping a more sustainable, inclusive future.
+              From providing reliable transportation solutions to empowering
+              communities through technology, we’re committed to delivering
+              excellence every step of the way.
             </p>
           </div>
         </div>
@@ -109,19 +117,25 @@ const Culture = () => {
           <Image src={culture} alt="people in a meeting" />
           <div className=" flex flex-col justify-center md:max-w-[50%]">
             <p className="font-bold text-xl pt-6 md:pt-0 md:text-3xl font-opensans">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod
+              Collaboration in Action
             </p>
             <p className="hidden md:block text-base md:text-xl text-black pt-6 font-opensans">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut{" "}
+              At Bold, teamwork drives everything we do, from brainstorming
+              sessions to rides across the city. We believe in the power of
+              collaboration to overcome challenges and unlock new opportunities.
+              Our open culture fosters innovation by welcoming fresh ideas,
+              respect by valuing every unique perspective, and accountability by
+              owning our actions and successes. Together, we create an
+              environment where bold thinking leads to impactful results.{" "}
             </p>
             <p className="md:hidden text-base text-black pt-6 font-opensans">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation
+              At Bold, teamwork drives everything we do, from brainstorming
+              sessions to rides across the city. We believe in the power of
+              collaboration to overcome challenges and unlock new opportunities.
+              Our open culture fosters innovation by welcoming fresh ideas,
+              respect by valuing every unique perspective, and accountability by
+              owning our actions and successes. Together, we create an
+              environment where bold thinking leads to impactful results.
             </p>
           </div>
         </div>
@@ -136,16 +150,16 @@ const Culture = () => {
           />
           <div className="top-0 left-0 h-[40vh] md:h-[90vh] md:max-w-[50%] pl-[5%] flex flex-col justify-center md:mb-24 z-20">
             <p className="font-bold text-[20px] md:text-[50px] md:leading-[70px] text-white">
-              Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit
+              Experience the Bold Difference. Download The Bold App Today!
             </p>
             <p className="text-[14px] text-white md:text-[20px] mt-3">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut
+              Our culture is about connecting, innovating, and making an impact
+              every day. Join us as a rider or driver and be part of our
+              commitment to sustainability, efficiency, and inclusivity.
+              Download the Bold app today and help shape the future of mobility!
             </p>
             <button className="rounded-lg mt-3 md:mt-10 md:py-3 md:px-6 py-2 px-4 font-redhat text-base md:text-xl font-semibold text-white bg-[#18C4B8] hover:bg-[#12a096] w-fit">
-              Download App
+              Download Now
             </button>
           </div>
         </div>
@@ -191,16 +205,14 @@ const Culture = () => {
         <div className="flex flex-col-reverse gap-2 md:gap-6 md:flex-row md:justify-between px-[5%] md:items-center pt-8 md:pt-12 md:pb-12 lg:pb-20 ">
           <div className="md:max-w-[50%] flex flex-col justify-center">
             <p className="font-bold text-xl font-opensans md:text-3xl lg:text-4xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor.
+              Passion Meets Purpose
             </p>
             <p className="text-sm md:text-xl pt-2 md:pt-4 lg:pt-6 text-[#777777]">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt.
+              We go beyond just providing services. We aim to make a difference.
+              Our people are passionate about sustainability, safety, and making
+              mobility accessible to everyone. From cycling initiatives to
+              innovative app features, Bold’s team is dedicated to developing
+              sustainable transportation solutions.
             </p>
           </div>
           <Image
@@ -213,13 +225,12 @@ const Culture = () => {
         <div className="flex flex-col-reverse gap-2 md:gap-6 md:flex-row-reverse md:justify-between px-[5%] md:items-center pt-8 md:pt-12 md:pb-12 lg:pb-20 ">
           <div className="md:max-w-[50%] flex flex-col justify-center">
             <p className="font-bold text-xl font-opensans md:text-3xl lg:text-4xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor.
+              We’re committed to Innovation, Inclusivity, And Sustainability
             </p>
             <p className="text-sm md:text-xl pt-2 md:pt-4 lg:pt-6 text-[#777777]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco
+              Our core values guide us. Innovation drives eco-friendly
+              solutions, inclusivity fosters a diverse, welcoming culture, and
+              sustainability reflects our commitment to a healthier planet.
             </p>
           </div>
           <Image
--- src/app/home/Footer.jsx
+++ src/app/home/Footer.jsx
@@ -8,57 +8,103 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="">
-        <div className="px-[5%] pt-8 pb-8  md:pb-20 lg:pb-28 bg-[#111111] flex flex-col md:flex-row justify-between gap-6 xl:gap-6 ">
-            <div className="md:max-w-[22%]">
-                <Image src={logo} alt="logo" className="w-20 sm:w-40"></Image>
-                <p className="font-semibold text-sm md:text-lg lg:text-xl text-white ">BOLD offers fast, comfortable, and eco-friendly ride options. Whether you are looking for spacious group rides, quick trips, or premium travel, BOLD delivers style and convenience. Enjoy the freedom to move your way!</p>
+      <div className="px-[5%] pt-8 pb-8  md:pb-20 lg:pb-28 bg-[#111111] flex flex-col md:flex-row justify-between gap-6 xl:gap-6 ">
+        <div className="md:max-w-[22%]">
+          <Image src={logo} alt="logo" className="w-20 sm:w-40"></Image>
+          <p className="font-semibold text-sm md:text-lg lg:text-xl text-white ">
+            BOLD offers fast, comfortable, and eco-friendly ride options.
+            Whether you are looking for spacious group rides, quick trips, or
+            premium travel, BOLD delivers style and convenience. Enjoy the
+            freedom to move your way!
+          </p>
+        </div>
+        <div className="flex items-end ">
+          <div className="flex justify-between gap-6 sm:gap-[3%] xl:gap-[7%] pt-2 sm:pt-6 md:pt-12 lg:pt-20 flex-col sm:flex-row sm:flex-wrap lg:flex-nowrap w-[80%] sm:w-full">
+            <div className="">
+              <p className="font-bold text-xl text-white">Contact Info</p>
+              <Link href="mailto:service@unanimeplanet.com">
+                <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-10">
+                  service@unanimeplanet.com
+                </p>
+              </Link>
             </div>
-            <div className="flex items-end ">
-                <div className="flex justify-between gap-6 sm:gap-[3%] xl:gap-[7%] pt-2 sm:pt-6 md:pt-12 lg:pt-20 flex-col sm:flex-row sm:flex-wrap lg:flex-nowrap w-[80%] sm:w-full">
-                    <div className="">
-                        <p className="font-bold text-xl text-white">Contact Info</p>
-                        <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-10">service@unanimeplanet.com</p>
-                    </div>
-                    <div className="min-w-[15%]">
-                        <p className="font-bold text-xl text-white">Important Links</p>
-                        <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-10">Our products</p>
-                        <Link href="/privacy">
-                        <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-6">Privacy policy</p>
-                        </Link>
-                        <Link href="/termsandcondition">
-                        <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-6">Terms & Conditions</p>
-                        </Link>
-                    </div>
-                    <div className="">
-                        <p className="font-bold text-xl text-white">Products</p>
-                        <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-10">Bold Ride</p>
-                        <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-6">Bold Drive</p>
-                        <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-6">Logistics</p>
-                    </div>
+            <div className="min-w-[15%]">
+              <p className="font-bold text-xl text-white">Important Links</p>
+              <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-10">
+                Our products
+              </p>
+              <Link href="/privacy">
+                <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-6">
+                  Privacy policy
+                </p>
+              </Link>
+              <Link href="/termsandcondition">
+                <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-6">
+                  Terms & Conditions
+                </p>
+              </Link>
+            </div>
+            <div className="">
+              <p className="font-bold text-xl text-white">Products</p>
+              <Link href="/ride">
+                <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-10">
+                  Bold Ride
+                </p>
+              </Link>
+              <Link href="/drive">
+                <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-6">
+                  Bold Drive
+                </p>
+              </Link>
+              <Link href="/logistics">
+                <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-6">
+                  Logistics
+                </p>
+              </Link>
+            </div>
 
-                    <div className="">
-                    <p className="font-bold text-xl text-white">Newsletter</p>
-                    <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-10">Stay updated with our newsletter and receive the latest news and offers.</p>
-                    <div className=" p-2 mt-8 rounded-3xl overflow-hidden bg-white flex justify-between">
-                        <input type="text" placeholder="Email address" className="max-w-[70%] px-4 outline-none"/>
-                        <button className="bg-[#18C4B8] rounded-full flex justify-center items-center h-10 w-10" onClick={()=> alert("Email added to Mailing list successfully!")}>
-                            <Image src={subscribeemail} alt="subscribe" className="w-4 h-4 sm:h-6 sm:w-6"></Image>
-                        </button>
-                    </div>
-                    </div>
-                </div>
+            <div className="">
+              <p className="font-bold text-xl text-white">Newsletter</p>
+              <p className="font-redhat sm:text-lg text-white text-sm font-semibold sm:font-normal pt-10">
+                Stay updated with our newsletter and receive the latest news and
+                offers.
+              </p>
+              <div className=" p-2 mt-8 rounded-3xl overflow-hidden bg-white flex justify-between">
+                <input
+                  type="text"
+                  placeholder="Email address"
+                  className="max-w-[70%] px-4 outline-none"
+                />
+                <button
+                  className="bg-[#18C4B8] rounded-full flex justify-center items-center h-10 w-10"
+                  onClick={() =>
+                    alert("Email added to Mailing list successfully!")
+                  }
+                >
+                  <Image
+                    src={subscribeemail}
+                    alt="subscribe"
+                    className="w-4 h-4 sm:h-6 sm:w-6"
+                  ></Image>
+                </button>
+              </div>
             </div>
+          </div>
         </div>
-        <div className="flex justify-between bg-[#000000] px-[5%] pt-6 pb-24 sm:py-6  flex-wrap gap-6 ">
-            <p className="text-sm text-white">Copyright 2022 | All rights reserved. </p>
-            <div className="flex items-center gap-12 ">
-                <p className=" font-semibold text-xl  md:font-normal md:text-sm  text-white">Follow us on: </p>
-                <Image src={socials} alt="social"/>
-            </div>
+      </div>
+      <div className="flex justify-between bg-[#000000] px-[5%] pt-6 pb-24 sm:py-6  flex-wrap gap-6 ">
+        <p className="text-sm text-white">
+          Copyright 2022 | All rights reserved.{" "}
+        </p>
+        <div className="flex items-center gap-12 ">
+          <p className=" font-semibold text-xl  md:font-normal md:text-sm  text-white">
+            Follow us on:{" "}
+          </p>
+          <Image src={socials} alt="social" />
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
